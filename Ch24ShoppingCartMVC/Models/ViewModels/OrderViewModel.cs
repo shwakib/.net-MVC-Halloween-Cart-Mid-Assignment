452a50c96159e546412a2b3e27187c9ba2280acc
Sifat Hasan Wakib <sifat6460@gmail.com>
2020-08-04 14:31:57 +0600

    multiplecart

---
+++ Ch24ShoppingCartMVC/Models/ViewModels/OrderViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Web.Mvc;
+
+namespace Ch24ShoppingCartMVC.Models
+{
+    public class OrderViewModel
+    {
+        public SelectList ProductsList { get; set; }
+        public ProductViewModel SelectedProduct { get; set; }
+    }
+}
